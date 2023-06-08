4841b7bc7bd1179d99bf88c5a481240402e17b9d
aaronnzhang <zhangyihao319@gmail.com>
2023-06-08 22:43:24 +0800

    [update] refine message ui

--- app/src/main/java/com/example/droidchat/Chat.kt
+++ app/src/main/java/com/example/droidchat/Chat.kt
@@ -17,25 +17,23 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.droidchat.component.DroidChatAppBar
-import kotlinx.coroutines.launch
+import com.example.droidchat.theme.DroidChatTheme
 
 @Composable
 fun ChatScreen() {
     var text by remember { mutableStateOf("") }
     val messages = remember { mutableStateListOf<Message>() }
     val listState = rememberLazyListState()
-    val coroutineScope = rememberCoroutineScope()
 
     Scaffold(
         topBar = { DroidChatAppBar(title = { Text("Droid Chat") }) }
@@ -74,12 +72,6 @@ fun ChatScreen() {
                 }
             }
         }
-
-        LaunchedEffect(messages.size) {
-            coroutineScope.launch {
-                listState.animateScrollToItem(0)
-            }
-        }
     }
 }
 
@@ -89,15 +81,30 @@ fun MessageView(message: Message) {
     CompositionLocalProvider(LocalTextStyle provides textStyle) {
         Card(
             modifier = Modifier
-                .padding(horizontal = 8.dp, vertical = 4.dp)
+                .padding(48.dp, 8.dp, 12.dp, 8.dp)
                 .fillMaxWidth()
                 .wrapContentWidth(if (message.isUser) Alignment.End else Alignment.Start),
-            shape = RoundedCornerShape(10.dp)
+            shape = RoundedCornerShape(20.dp)
         ) {
             Text(
                 text = message.content,
-                modifier = Modifier.padding(8.dp),
+                modifier = Modifier.padding(16.dp, 12.dp),
             )
         }
+    }
+}
+
+@Preview
+@Composable
+fun MessagePreview() {
+    DroidChatTheme {
+        MessageView(
+            message = Message(
+                content = "得益于苹果的广泛关注度，原本定位于开发者技术会议的 WWDC，其关注面越发延伸到一般技术爱好者甚至大众用户。不过，人们的关注点一般都聚焦在开场的主题演讲（Keynote）上，认为其余活动都只是和职业开发者相关的技术交流。\n" +
+                        "\n" +
+                        "实际上，在 WWDC 的一百多场讲座（session）中，真正「捋起袖子写代码」的场景并不占大多数。相反，很多讲座都会介绍开发和设计的相关基础知识，并辅以大量生动有趣的演示。即使不从事开发工作的爱好者，也可以借观看 WWDC 讲座，快人一步地了解苹果最新的功能规划、生态布局，更加深入地理解技术背后的原理和动机。\n",
+                isUser = true
+            )
+        )
     }
 }
--- app/src/main/java/com/example/droidchat/MainActivity.kt
+++ app/src/main/java/com/example/droidchat/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import com.example.droidchat.theme.DroidChatTheme
 
 class MainActivity : AppCompatActivity() {
@@ -17,10 +14,7 @@ class MainActivity : AppCompatActivity() {
             ComposeView(this).apply {
                 setContent {
                     DroidChatTheme {
-                        Surface(
-                            color = MaterialTheme.colorScheme.background,
-                            modifier = Modifier.nestedScroll(rememberNestedScrollInteropConnection())
-                        ) {
+                        Surface(color = MaterialTheme.colorScheme.background) {
                             ChatScreen()
                         }
                     }
